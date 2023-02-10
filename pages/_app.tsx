5f3643426b34f7b64481a33c35c7fe165c12d5de
Robbie Mariano <robbie@roofbuddy.co.nz>
2023-02-10 16:07:57 +0800

    feat: Implement part two

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
-import '../styles/tailwind.css';
-import Layout from '../components/Layout';
+import { ApolloProvider } from "@apollo/client/react";
+import { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import apolloClient from "../lib/apollo";
+import "../styles/tailwind.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={apolloClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,35 @@
-import Head from 'next/head';
-import { AwesomeLink } from '../components/AwesomeLink';
-import { links } from '../data/links';
+import { gql } from "@apollo/client/core";
+import { useQuery } from "@apollo/client/react/hooks";
+import type { Link } from "@prisma/client";
+import Head from "next/head";
+import { AwesomeLink } from "../components/AwesomeLink";
+import { links } from "../data/links";
+
+const AllLinksQuery = gql`
+  query {
+    links {
+      id
+      title
+      url
+      description
+      imageUrl
+      category
+    }
+  }
+`;
 
 export default function Home() {
+  const { data, loading, error, fetchMore } = useQuery(AllLinksQuery, {
+    variables: { first: 2 },
+  });
+
+  console.log(data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
+
+  const { endCursor, hasNextPage } = data.links.pageInfo;
+
   return (
     <div>
       <Head>
@@ -11,19 +38,44 @@ export default function Home() {
       </Head>
 
       <div className="container mx-auto max-w-5xl my-20">
-        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {links.map((link) => (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {data?.links.edges.map(({ node }: { node: Link }) => (
             <AwesomeLink
-              key={link.id}
-              url={link.url}
-              id={link.id}
-              category={link.category}
-              title={link.title}
-              description={link.description}
-              imageUrl={link.imageUrl}
+              key={node.id}
+              url={node.url}
+              id={node.id}
+              category={node.category}
+              title={node.title}
+              description={node.description}
+              imageUrl={node.imageUrl}
             />
           ))}
-        </ul>
+        </div>
+
+        {hasNextPage ? (
+          <button
+            className="px-4 py-2 bg-blue-500 text-white rounded my-10"
+            onClick={() => {
+              fetchMore({
+                variables: { after: endCursor },
+                updateQuery: (prevResult, { fetchMoreResult }) => {
+                  fetchMoreResult.links.edges = [
+                    ...prevResult.links.edges,
+                    ...fetchMoreResult.links.edges,
+                  ];
+
+                  return fetchMoreResult;
+                },
+              });
+            }}
+          >
+            more
+          </button>
+        ) : (
+          <p className="my-10 text-center font-medium">
+            You've reached the end!
+          </p>
+        )}
       </div>
     </div>
   );
